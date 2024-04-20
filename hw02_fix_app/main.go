128efee55b6698b6737cf2112293fe7d4efcaedb
Aleksandra <aleksandra.goltsova@tabby.ai>
2024-04-20 13:16:19 +0400

    HW2 is finished

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/Alenary/otus-go-basic/hw02_fix_app/printer"
 	"github.com/Alenary/otus-go-basic/hw02_fix_app/reader"
 	"github.com/Alenary/otus-go-basic/hw02_fix_app/types"
 )
 
 func main() {
-	var path = "data.json"
+	path := "data.json"
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
@@ -17,7 +18,7 @@ func main() {
 	var staff []types.Employee
 
 	if len(path) == 0 {
-		path  = "data.json"
+		path = "data.json"
 	}
 
 	staff, err = reader.ReadJSON(path, -1)
