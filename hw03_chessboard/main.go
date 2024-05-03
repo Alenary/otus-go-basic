d3d3ae5d4a7383fbcf73e162a016d2c0358ea693
Aleksandra <aleksandra.goltsova@tabby.ai>
2024-05-03 16:39:25 +0400

    HW2 is completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,56 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var size int
+	fmt.Println("Введите число - размер шахматной доски")
+	fmt.Scanf("%d", &size)
+	if size <2 {
+		fmt.Println("Указаны некорректные данные")
+	} else {
+		for i := 0; i < size; i++ {
+			if i%2 == 0 {
+				createchessline1(size)
+			} else {
+				createchessline2(size)
+			}
+		}
+	}
+}
+
+func createchessline1(size int) {
+	for i := 0; i < size; i++ {
+		if i < size-1 {
+			if i%2 == 0 {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+		} else {
+			if i%2 == 0 {
+				fmt.Println("#")
+			} else {
+				fmt.Println(" ")
+			}
+		}
+	}
+}
+
+func createchessline2(size int) {
+	for i := 0; i < size; i++ {
+		if i < size-1 {
+			if i%2 == 0 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print("#")
+			}
+		} else {
+			if i%2 == 0 {
+				fmt.Println(" ")
+			} else {
+				fmt.Println("#")
+			}
+		}
+	}
 }
