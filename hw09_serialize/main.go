40de0b6cbae9f826afc3840b2404a207a6ccb5e9
Aleksandra <aleksandra.goltsova@tabby.ai>
2024-07-09 18:53:53 +0400

    HW09 is added

--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -1,5 +1,74 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"google.golang.org/protobuf/proto"
+)
+
+// SerializeBooks сериализует слайс объектов Book в байтовый массив (Protobuf).
+func SerializeBooks(books []*Book) ([]byte, error) {
+	bookList := &BookList{
+		Books: books,
+	}
+	return proto.Marshal(bookList)
+}
+
+// DeserializeBooks десериализует байтовый массив в слайс объектов Book (Protobuf).
+func DeserializeBooks(data []byte) ([]*Book, error) {
+	bookList := &BookList{}
+	err := proto.Unmarshal(data, bookList)
+	if err != nil {
+		return nil, err
+	}
+	return bookList.Books, nil
+}
+
+// SerializeBooksJSON сериализует слайс объектов Book в JSON.
+func SerializeBooksJSON(books []*Book) ([]byte, error) {
+	return json.Marshal(books)
+}
+
+// DeserializeBooksJSON десериализует JSON в слайс объектов Book.
+func DeserializeBooksJSON(data []byte) ([]*Book, error) {
+	var books []*Book
+	err := json.Unmarshal(data, &books)
+	return books, err
+}
+
 func main() {
-	// Place your code here.
+	books := []*Book{
+		{Id: 1, Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Year: 1925, Size: 180, Rate: 4.5},
+		{Id: 2, Title: "1984", Author: "George Orwell", Year: 1949, Size: 328, Rate: 4.8},
+	}
+
+	data, err := SerializeBooks(books)
+	if err != nil {
+		log.Fatalf("Error serializing books: %v", err)
+	}
+
+	fmt.Println("Serialized books (Protobuf):", data)
+
+	deserializedBooks, err := DeserializeBooks(data)
+	if err != nil {
+		log.Fatalf("Error deserializing books: %v", err)
+	}
+
+	fmt.Println("Deserialized books (Protobuf):", deserializedBooks)
+
+	jsonData, err := SerializeBooksJSON(books)
+	if err != nil {
+		log.Fatalf("Error serializing books to JSON: %v", err)
+	}
+
+	fmt.Println("Serialized books (JSON):", string(jsonData))
+
+	deserializedBooksJSON, err := DeserializeBooksJSON(jsonData)
+	if err != nil {
+		log.Fatalf("Error deserializing books from JSON: %v", err)
+	}
+
+	fmt.Println("Deserialized books (JSON):", deserializedBooksJSON)
 }
